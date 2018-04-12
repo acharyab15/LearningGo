09c22685350007722e7d5ea192365aa34862c018
Bipeen <acharyab@seas.upenn.edu>
2018-04-11 21:54:35 -0700

    news aggregation and data tables and html templating and maps

--- looping.go
+++ looping.go
@@ -6,31 +6,32 @@ import 	("fmt"
 	"encoding/xml")
 
 type SitemapIndex struct {
-	// Locations is the value which is an array of the location type
-	// Location is in xml under sitemap tag (for unmarshalling)
-	Locations []Location `xml:"sitemap"`
+	// Locations is the value which is a slice of the string type
+	// of Locations which is in xml under sitemap tag (for unmarshalling)
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keyworkds"`
+	Locations []string `xml:"url>loc"`
 }
 
 func main() {
+	var s SitemapIndex
+	var n News
+
 	resp, _ := http.Get("http://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
-	var s SitemapIndex
 	// bytes is what we unmarshal
 	// we unmarshal to memory of s
 	xml.Unmarshal(bytes, &s)
 	
 	for _, Location := range s.Locations {
-		fmt.Printf("\n%s", Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
 	}
-
 }
--- simple_web_app.go
+++ simple_web_app.go
@@ -1,18 +1,67 @@
 package main
 
 import ("fmt"
-	"net/http")
+	"net/http"
+	"io/ioutil"
+	"encoding/xml"
+	"html/template")
 
+type SitemapIndex struct {
+	// Locations is the value which is a slice of the string type
+	// of Locations which is in xml under sitemap tag (for unmarshalling)
+	Locations []string `xml:"sitemap>loc"`
+}
 
-func index_handler(w http.ResponseWriter, r *http.Request){
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News map[string]NewsMap
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request){
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string]NewsMap)
+
+	resp, _ := http.Get("http://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	// bytes is what we unmarshal
+	// we unmarshal to memory of s
+	xml.Unmarshal(bytes, &s)
+	
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+	t, _ := template.ParseFiles("basictemplating.html")
+	fmt.Println(t.Execute(w, p))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Whoa, Go is neat!")
 }
 
-func about_handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Expert Blockchain")
-}
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/about/", about_handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
 	http.ListenAndServe(":8000", nil)
 }
